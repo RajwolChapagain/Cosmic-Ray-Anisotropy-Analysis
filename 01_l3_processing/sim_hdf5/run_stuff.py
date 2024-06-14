39df25c51b22e30d3a65af2856e4e0bd71384fab
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2024-06-14 08:54:55 -0500

    Rename input file

--- 01_l3_processing/sim_hdf5/run_stuff.py
+++ 01_l3_processing/sim_hdf5/run_stuff.py
@@ -2,7 +2,7 @@ import os, fileinput, sys
 
 
 def runSim(particle, fnum):
-	for line in fileinput.input('steeringcard_sim',inplace=1):
+	for line in fileinput.input('steeringcard_dstsim',inplace=1):
 		arg_string = particle + ' ' + str(fnum)
 		if 'arguments' in line:
 			line = line.replace(line, 'arguments = '+arg_string+'\n')
