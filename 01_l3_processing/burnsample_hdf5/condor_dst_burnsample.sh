ab61245c45bca2c4850e7dc3a6f7213f53acb2a0
Gunwati Agrawal <gagrawal@icecube.wisc.edu>
2024-06-27 18:08:23 -0500

    Updated burnsample_hdf5, fixing bugs and adding feature to create output directory.

--- 01_l3_processing/burnsample_hdf5/condor_dst_burnsample.sh
+++ 01_l3_processing/burnsample_hdf5/condor_dst_burnsample.sh
@@ -5,6 +5,8 @@
 #eval cvmfs
 #eval combo
 
+# Define the output directory
+OUTPUT_DIR="OUTPUT_DIR"
 
 TMPDIR=$(mktemp -d)
 cp l3_i3_burnsample_hdf.py $TMPDIR
@@ -12,6 +14,12 @@ cp l3_i3_burnsample_hdf.py $TMPDIR
 cd $TMPDIR
 python l3_i3_burnsample_hdf.py $1 $2 $3
 
-mv l3_data* /data/user/@USER_DIR/burnsamplehdf5
+# Create output directory if it does not already exist and notify user
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+  echo "Directory $OUTPUT_DIR created."
+else
+  echo "Directory $OUTPUT_DIR already exists."
+fi
 
-
+mv l3_data* $OUTPUT_DIR
--- 01_l3_processing/burnsample_hdf5/run_condor.py
+++ 01_l3_processing/burnsample_hdf5/run_condor.py
@@ -3,26 +3,24 @@ import os, fileinput, sys
 ##run this code in condor shell to submit condor job for each month of data (run through i3 convert py file)
 #this can be used for creating burnsample hdf5 and root files
 for run_year in range(2011, 2022):
-	for year in [run_year]:
-		for month in range(4,13):
-			for day in range(1, 32):
-               	for line in fileinput.input('steeringcard_dst',inplace=1):
-                    arg_string = str(run_year)+' '+str(year)+' '+str(month)+' '+str(day)
-                    		if 'arguments' in line:
-                            	line = line.replace(line,'arguments = '+arg_string+'\n')
-                                	sys.stdout.write(line)
-				os.system('condor_submit steeringcard_dstburnsample') 
-				print('running for', run_year, year, month, day)
+    for year in [run_year]:
+        for month in range(4,13):
+            for line in fileinput.input('steeringcard_dstburnsample',inplace=1):
+                arg_string = str(run_year)+' '+str(year)+' '+str(month)
+                if 'arguments' in line:
+                    line = line.replace(line,'arguments = '+arg_string+'\n')
+                sys.stdout.write(line)
+	    os.system('condor_submit steeringcard_dstburnsample') 
+	    print('running for', run_year, year, month)
 
 
-	for year in [run_year+1]:
-		for month in range(1, 6):
-			for day in range(1, 32):
-				for line in fileinput.input('steeringcard_dst',inplace=1):
-					arg_string = str(run_year)+' '+str(year)+' '+str(month)+' '+str(day)
-					if 'arguments' in line:
-		    				line = line.replace(line,'arguments = '+arg_string+'\n')
-					sys.stdout.write(line)
-				os.system('condor_submit steeringcard_dstburnsample')
-				print('running for', run_year, year, month, day)'''
+    for year in [run_year+1]:
+        for month in range(1, 8):
+            for line in fileinput.input('steeringcard_dstburnsample',inplace=1):
+	        arg_string = str(run_year)+' '+str(year)+' '+str(month)
+		if 'arguments' in line:
+		    line = line.replace(line,'arguments = '+arg_string+'\n')
+		sys.stdout.write(line)
+	    os.system('condor_submit steeringcard_dstburnsample')
+	    print('running for', run_year, year, month)
 
